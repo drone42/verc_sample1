f4b341f2edbc453da7865fd041fa772391ad760e
drone42 <62670721+drone42@users.noreply.github.com>
2022-12-10 16:56:51 +0100

    Delete script directory

--- script/src/nft.py
+++
@@ -1,7 +0,0 @@
-class NFT:
-    """represents an NFT"""
-    def __init__(self, name, uri, metadataUri, description):
-        self.name = name
-        self.uri = uri
-        self.metadataUri = metadataUri
-        self.description = description
